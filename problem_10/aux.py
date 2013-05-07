85ff384895b415488c11e7d8102b8a09afd5183a
Alonso Vidales <alonso.vidales@tras2.es>
2013-05-07 15:46:10 +0200

    Removed unnecesary files

--- problem_10/aux.py
+++
@@ -1,4 +0,0 @@
-f = open('tuenti_test', 'w')
-for count in xrange(0, 62):
-    f.write("a")
-f.close()
--- problem_10/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-    echo md5('abcccbccc') . PHP_EOL;
-    echo md5('10[abc]') . PHP_EOL;
-    echo md5('abcabcabcabcabcabcabcabcabcabc') . PHP_EOL;
-    echo "c53115f92f2cbee6cb25be4a42f03fba" . PHP_EOL;
-    echo "1b66558be2b2cd898ebc3383623a3677" . PHP_EOL;
